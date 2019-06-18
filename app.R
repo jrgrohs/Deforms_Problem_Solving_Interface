f18ac556bf6916f5914a819625b18f7885aa6bdf
Darren Maczka <dmaczka@vt.edu>
2019-06-18 16:39:25 -0400

    use packrat

--- app.R
+++ app.R
@@ -7,9 +7,7 @@
 # source("https://install-github.me/zzawadz/dragulaR")  -  drag and drop library
 library(shiny)
 library(knitr)
-library(tidyverse)
-library(shinyWidgets)
-library(png)
+#library(shinyWidgets)
 library(dragulaR)
 
 # Define Variables 
@@ -19,10 +17,10 @@ var_list <- c("Unknown","50 mm","50 mm","1 mm","70 GPa","23 x 10-6 oC-1","100 oC
 # creates the div containers for the drag and drop
 makeElement <- function(data)
 {
-  name <- names(data)[[1]]
-  div(style = "border-width:2px;border-style:solid;",
-      drag = name,
-      div(class = "active content", withMathJax(paste0('\\(', data, '\\)'))))
+	name <- names(data)[[1]]
+	div(style = "border-width:2px;border-style:solid;",
+		drag = name,
+		div(class = "active content", withMathJax(paste0('\\(', data, '\\)'))))
 }
 
 
@@ -31,69 +29,56 @@ ui <- fluidPage(
   
   #fluidRow(column(5, offset = 4, titlePanel("Deforms Problem Solving Interface"))),
   titlePanel("Deforms Problem Solving Interface"),
-    
+  
   sidebarLayout( position = "right",
-    
-    sidebarPanel(
-      h4("Equation Bank"),
-      column(12,
-             h3("Drag from here:"),
-             uiOutput("equationList")
-             
-             # TODO: Fix Persistance in Drag and Drop elements
-             #    Should have to do with setting the copy variable true, 
-             #    but that documentation is only for html
-             #    https://sindu12jun.github.io/dragula/
-             # ,
-                 # lapply(colnames(mtcars), makeElement, data = mtcars)
-             # dragula([document.getElementById("Available")),
-             # document.getElementById("Available")],{
-             #   copy:true
-             # }
-        ),
-      dragulaOutput("dragula"),
-      
-      hr(),
-      
-      h4("Solution and Feedback")
-    ),
-    
-    
-    mainPanel(
-      fluidRow( 
-        column(12,
-          withMathJax(),
-          uiOutput("problemStatement")
-        )
-      ),
-      
-      # Space betweeen Problem and Workspace
-      br(),
-      br(),
-      
-      fluidRow(
-       #  column(4, 
-       #    wellPanel(
-       #      h4("Working Equations"),
-       #      dropdownButton(
-       #        tags$h3("Equation 1"), uiOutput("testing2"),
-       #        circle = FALSE, status = "primary", label=pluck(temp_change_formula,formula), 
-       #        icon = icon("calculator"), 
-       #        width = "300px",
-       #        tooltip = tooltipOptions(title = "Click to select your variables!")
-       #      ) 
-       #    )
-       #  ),
-
-      column(12,
-        h3("Work Space"),
-        column(12,
-          h3("Drop here:"),
-          div(id = "Model", style = "min-height: 600px;")
-          )
-        )
-      )
-    )
+                 
+                 sidebarPanel(
+                   h4("Equation Bank"),
+                   column(12,
+                          h3("Drag from here:"),
+                          uiOutput("equationList")
+                          
+                          # TODO: Fix Persistance in Drag and Drop elements
+                          #    Should have to do with setting the copy variable true, 
+                          #    but that documentation is only for html
+                          #    https://sindu12jun.github.io/dragula/
+                          # ,
+                          # lapply(colnames(mtcars), makeElement, data = mtcars)
+                          # dragula([document.getElementById("Available")),
+                          # document.getElementById("Available")],{
+                          #   copy:true
+                          # }
+                   ),
+                   dragulaOutput("dragula"),
+                   
+                   hr(),
+                   
+                   h4("Solution and Feedback")
+                 ),
+                 
+                 
+                 mainPanel(
+                   fluidRow( 
+                     column(12,
+                            withMathJax(),
+                            uiOutput("problemStatement")
+                     )
+                   ),
+                   
+                   # Space betweeen Problem and Workspace
+                   br(),
+                   br(),
+                   
+                   fluidRow(
+                     column(12,
+                            h3("Work Space"),
+                            column(12,
+                                   h3("Drop here:"),
+                                   div(id = "Model", style = "min-height: 600px;")
+                            )
+                     )
+                   )
+                 )
   )
 )
 
@@ -101,7 +86,7 @@ ui <- fluidPage(
 # Define Server Function
 
 server <- function(input, output) {
-   
+  
   selectedProblem <- file.path('problems', 'problem1.md')
   
   output$problemStatement <- renderUI({
@@ -111,9 +96,9 @@ server <- function(input, output) {
   output$equationList <- renderUI({
     frontMatter <- rmarkdown::yaml_front_matter(selectedProblem)
     div(id = "Available", style = "min-height: 250px;", 
-      lapply(frontMatter$equations, makeElement))
+        lapply(frontMatter$equations, makeElement))
   })
-
+  
   # Dragula Functions
   output$dragula <- renderDragula({
     dragula(c("Available", "Model"))
