d585858225c21a9737747c3ae6e2023b1d66264d
Darren Maczka <dmaczka@vt.edu>
2019-06-18 15:39:06 -0400

    move problem definition to markdown file; fix math rendering in equation list

--- app.R
+++ app.R
@@ -6,37 +6,23 @@
 # Libraries ----
 # source("https://install-github.me/zzawadz/dragulaR")  -  drag and drop library
 library(shiny)
+library(knitr)
 library(tidyverse)
 library(shinyWidgets)
 library(png)
 library(dragulaR)
 
-
-# Define Formulas
-# Used for drag and drop equations
-# TODO: Get the math to display properly
-#   played around with Mathjax https://github.com/mathjax/mathjax
-equation_1 <- c("delta=delta_T * l * $(\\alpha$)","$(\\beta$)")
-equation_2 <- c("equation 2")
-equation_3 <- c("equation 3")
-physics_equations_df <- data.frame(equation_1, 
-                                   equation_2,
-                                   equation_3)
-
 # Define Variables 
 var_list <- c("Unknown","50 mm","50 mm","1 mm","70 GPa","23 x 10-6 oC-1","100 oC")
-source("deforms_formulas.R")
-
 
 # Define makeElement
 # creates the div containers for the drag and drop
-makeElement <- function(data, name)
+makeElement <- function(data)
 {
+  name <- names(data)[[1]]
   div(style = "border-width:2px;border-style:solid;",
       drag = name,
-      div(class = "active content", p(sprintf("%s", data[[name]]))))
-      # math(data[[name]])
-      # div(class = "active content", p(sprintf("Class: %s", class(data[[name]])))))
+      div(class = "active content", withMathJax(paste0('\\(', data, '\\)'))))
 }
 
 
@@ -50,19 +36,9 @@ ui <- fluidPage(
     
     sidebarPanel(
       h4("Equation Bank"),
-      dropdownButton(
-        tags$h3("List of Equations"), 
-        uiOutput("testing2"),
-        circle = FALSE, status = "primary", 
-        label = pluck(temp_change_formula, formula),
-        icon = icon("calculator"), width = "300px",
-        tooltip = tooltipOptions(title = "Click to select your equations!")
-        ), 
-      
       column(12,
              h3("Drag from here:"),
-             div(id = "Available", style = "min-height: 250px;",
-                 lapply(colnames(physics_equations_df), makeElement, data = physics_equations_df))
+             uiOutput("equationList")
              
              # TODO: Fix Persistance in Drag and Drop elements
              #    Should have to do with setting the copy variable true, 
@@ -87,20 +63,9 @@ ui <- fluidPage(
       fluidRow( 
         column(12,
           withMathJax(),
-          h3("Problem 1"),
-          
-          # Deforms Problem
-          p("A solid square bar of cross-sectional dimensions  50mm x 50 mm is attached 
-                  rigidly to the wall at point A and has a gap of 1 mm  from the rigid wall 
-                  at point B at \\(\\ 25^\\circ C\\)."),
-          imageOutput("image1", height = "100%"),
-          
-          p("Knowing the bar is made of aluminum E = 70 GPa, \\(\\alpha = 23 * 10^{-6}\\) :"),
-          p("a.  At what temperature does the gap close?"),
-          p("b.  What stress exists in the bar at a temperature of \\(\\ 100^\\circ C \\)?")
+          uiOutput("problemStatement")
         )
       ),
-      
       
       # Space betweeen Problem and Workspace
       br(),
@@ -137,13 +102,18 @@ ui <- fluidPage(
 
 server <- function(input, output) {
    
-  output$image1 <- renderImage({
-    list(
-      src = "deforms_figure.png",
-      contentType = "image/png",
-      alt = "This is a figure representing the bar fixed at one end and gapped on the other")}, 
-    deleteFile=FALSE)
+  selectedProblem <- file.path('problems', 'problem1.md')
   
+  output$problemStatement <- renderUI({
+    HTML(markdown::markdownToHTML(knit(selectedProblem, quiet = TRUE)))
+  })
+  
+  output$equationList <- renderUI({
+    frontMatter <- rmarkdown::yaml_front_matter(selectedProblem)
+    div(id = "Available", style = "min-height: 250px;", 
+      lapply(frontMatter$equations, makeElement))
+  })
+
   output$testing = renderUI({
     withMathJax() 
     materialSwitch(
@@ -176,8 +146,6 @@ server <- function(input, output) {
   })
   
 }
-
-
 
 # Run the application
 
---
+++ problems/problem1.md
@@ -0,0 +1,15 @@
+---
+title: Temperature Expansion
+equations:
+  - temp_change: "\\delta_t = \\alpha \\Delta T L"
+  - eq2: "\\beta"
+---
+
+A solid square bar of cross-sectional dimensions  50mm x 50 mm is attached rigidly to the wall at point A and has a gap of 1 mm  from the rigid wall at point B at $25^\circ C$.  
+
+![rigid bar attached to wall A, with a small gap between right edge of bar and wall B](deforms_figure.png)
+
+Knowing the bar is made of aluminum $E = 70 GPa$, $\alpha = 23 * 10^{-6}$ :
+    a.  At what temperature does the gap close? 
+    b.  What stress exists in the bar at a temperature of $100^\\circ C$?'
+    
